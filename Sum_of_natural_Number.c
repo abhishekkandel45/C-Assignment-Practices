0e73d7b07f01857d2b597e1ba30085ec15b4927a
abhishekkandel45 <abhishekkandel45@gmail.com>
2022-01-09 16:35:19 +0530

    // Write a C program to find the Sum of Natural Number from 1 to n using for loop
    // Write a C program to find the Sum of Natural Number from 1 to n using for loop

---
+++ Sum_of_natural_Number.c
@@ -0,0 +1,14 @@
+// Write a C program to find the Sum of Natural Number from 1 to n using for loop
+#include <stdio.h>
+int main()
+{
+int n, sum=0, i;
+ printf ("Enter the Number to find Sum of natural Number:  ");
+ scanf ("%d",&n);
+ for (i = 1; i <=n; i++)
+ {
+     sum=sum+i;
+ }
+ printf ("\n The sum of natural number from 1 to %d is %d",n,sum);
+ return 0;
+}
