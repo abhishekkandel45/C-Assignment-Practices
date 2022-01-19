4ea008861306208eb1f34766fe46893f3da884b7
abhishekkandel45 <abhishekkandel45@gmail.com>
2022-01-19 23:59:55 +0530

    asasadssf

--- areaoftringle.c
+++ areaoftringle.c
@@ -1,20 +1,10 @@
-/*
-To evaluate area of Triangle (sqrt(s(s-a)(s-b)(s-c)
-*/
 #include <stdio.h>
-#include <math.h>
-#include <conio.h>
-
-void main(){
-    float a, b, c, s, area;
-    printf("Enter the length of three sides of triangle\n");
-    scanf("%f %f %f", &a, &b, &c);
-    /* using the formula
-    s=(a+b+c)/2
-    (sqrt(s(s-a)(s-b)(s-c)  */
-    s = (a + b + c)/2;
-    area = sqrt(s*(s-a)*(s-b)*(s-c));
-    printf("Area of triangle : %0.4f\n", area);
-    return 0;
-    getch();
+int main()
+{
+    int marks[4];
+    for (  i = 0; i < 4; i++)
+    {
+        print(" Enter the %d is ")
+    }
+    
 }
