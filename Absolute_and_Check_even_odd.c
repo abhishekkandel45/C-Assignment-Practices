c7a0048f7856e354f434bcb4c143011289cf4f3a
abhishekkandel45 <abhishekkandel45@gmail.com>
2022-01-24 22:40:11 +0530

    Write a C program to input n numbers in an array, calculate the sum of all even numbers and all  odd numbers in the array and print the larger sum.  Example:  If the array contains the following elements:  2, 3, 3, 5, 4, 8, 7, 11, 2  The sum of all even elements is 2+4+8+2=16  Sum of all odd elements is 3+3+5+7+11=29  Therefore, the output should be 29.
     Write a C program to input n numbers in an array, calculate the sum of all even numbers and all
    odd numbers in the array and print the larger sum.
    Example:
    If the array contains the following elements:
    2, 3, 3, 5, 4, 8, 7, 11, 2
    The sum of all even elements is 2+4+8+2=16
    Sum of all odd elements is 3+3+5+7+11=29
    Therefore, the output should be 29.

--- Absolute_and_Check_even_odd.c
+++ Absolute_and_Check_even_odd.c
@@ -1,32 +1,60 @@
- #include<stdio.h>
-  int evenodd(int);
-  int absolute(int);
-  int main()
-  {
-        int number,remainder;
-        printf(" Enter an integer number: ");
-        scanf("%d",&number);
-        printf("Absolute of %d is  %d\n",number,absolute(number));
-        evenodd(number);
-        return(0);
-    }
-
-  int evenodd(int number)
-  {
-        int remainder;
-        remainder = absolute(number) % 2;
-        switch(remainder){
-                case 0 : printf("\n %d is an even number.",number);
-                break;
-                case 1 : printf("\n %d is an odd number.",number);
-                break;
-    }
-    return 0;
+/*
+Q10. Write a C program to input n numbers in an array, calculate the sum of all even numbers and all 
+odd numbers in the array and print the larger sum. 
+Example: 
+If the array contains the following elements: 
+2, 3, 3, 5, 4, 8, 7, 11, 2 
+The sum of all even elements is 2+4+8+2=16 
+Sum of all odd elements is 3+3+5+7+11=29 
+Therefore, the output should be 29.
+*/
+#include <stdio.h>
+int printSumOfArray (int arr[], int size){
+	int i;
+	int sum=0;
+	for(int i=0; i<size; i++){
+		sum+=arr[i];
+		if(i==0) printf("%d", arr[i]);
+		else printf("+%d", arr[i]);
+	}
+	printf("=%d", sum);
+	return sum;
 }
-int absolute(int n)
-{
-    if(n < 0)            // if number is negative, say -n
-    return -n;   // return -(-n) which is +n.
-    
-    return n;           // Otherwise, return n.
-} 
+
+int main(void) {
+  int n,i, inp;
+  printf("Enter total number in array: ");
+  scanf("%d", &n);
+  int arr[n];
+  int odd_arr[n], odd_size = 0, sum_odd=0;
+  int even_arr[n], even_size = 0, sum_even=0;
+  int sum_max=0;
+
+  printf("Enter numbers [n1 n2 n3]: ");
+  for(i=0; i<n; i++){
+    scanf("%d", &arr[i]);
+  }
+
+  for (i=0; i<n; i++){
+    inp = arr[i];
+    if(inp%2==0) {
+		  even_arr[even_size] = inp;
+		  even_size++;
+	  }else {
+	  	odd_arr[odd_size] = inp;
+  		odd_size++;
+	  }
+  }
+
+  printf("\nThe sum of all even elements is:");
+  sum_even = printSumOfArray(even_arr, even_size);
+
+  printf("\nSum of all odd elements is:");
+  sum_odd = printSumOfArray(odd_arr, odd_size);
+
+  sum_max = sum_even > sum_odd ? sum_even : sum_odd;
+
+  printf("\nTherefore, the output should be %d. \n", sum_max);
+
+  return 0;
+}
