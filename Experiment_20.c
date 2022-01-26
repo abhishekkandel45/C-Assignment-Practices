ec9dc42f29ffcbd799ccaa9e1bb6ba249cf778a6
abhishekkandel45 <abhishekkandel45@gmail.com>
2022-01-26 11:08:11 +0530

    // Write a program to perform the string functions
    // Write a program to perform the string functions.

---
+++ Experiment_20.c
@@ -0,0 +1,11 @@
+// Write a program to perform the string functions.
+#include <stdio.h>
+int main()
+{
+    char name [30];
+    printf ("Please Enter Name: ");
+
+    gets (name); //gets is used to read the line
+    printf ("The name you Enterd was: %s",name);
+    return 0;
+}
